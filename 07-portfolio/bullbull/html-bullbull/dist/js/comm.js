ae9f15c1a162f8e0f7a2af40e0ec2e670f647081
eehd80 <eehd80yul@gmail.com>
2020-11-26 12:50:52 +0900

    bull prize

--- 07-portfolio/bullbull/html-bullbull/dist/js/comm.js
+++ 07-portfolio/bullbull/html-bullbull/dist/js/comm.js
@@ -1,15 +1,20 @@
 $(function(){
 
   // 스크롤 시 .navbar 디자인 변경
-  $(window).scroll(function(){
-    if($(this).scrollTop() > 40){
-      $('.navbar').addClass('sticky')
-    }else{
-      $('.navbar').removeClass('sticky')
-    }
-
+  $('.navbar').each(function(){
+    
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 40){
+        $('.navbar').addClass('sticky')
+      }else{
+        $('.navbar').removeClass('sticky')
+      }
+    })
+    // 윈도우 스크롤 이벤트를 발생시키시오.
+    // (header의 초기 디자인을 조정하기 위해)
+    $(window).trigger('scroll');
   })
-
+  
 
 
 })
