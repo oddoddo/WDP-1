785c501dc27a8daa10a9329e00ff5cd73e806527
eehd80 <eehd80yul@gmail.com>
2020-11-27 10:53:11 +0900

    bull jquery

--- 07-portfolio/bullbull/html-bullbull/dist/js/comm.js
+++ 07-portfolio/bullbull/html-bullbull/dist/js/comm.js
@@ -15,5 +15,23 @@ $(function(){
     $(window).trigger('scroll');
   })
 
+  // prizes click event
+  $('.lst-prize').each(function(){
+
+    // .lst-prize 첫번째 li에 .on 추가
+    $('.lst-prize > li:first-child').addClass('on')
+
+    // .lst-prize 두번째 li를 클릭했을 때... 기능을 넣어줘...
+    $('.lst-prize > li').eq(1).click(function(){
+      //.col-img img의 속성값 변경
+      $('.col-img img').attr('src','./images/img-jackpots1.gif')
+      // .lst-prize > li 의 .on 삭제
+      $('.lst-prize > li').removeClass('on')
+      // 방금 내가 클릭한 li에 .on 추가
+      $(this).addClass('on')
+    })
+
+  })
+
 
 })
