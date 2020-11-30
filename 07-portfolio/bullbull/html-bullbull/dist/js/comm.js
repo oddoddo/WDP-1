32da5844403b1931b4b683b13a0e9a4c8e51279d
eehd80 <eehd80yul@gmail.com>
2020-11-30 13:31:39 +0900

    slide

--- 07-portfolio/bullbull/html-bullbull/dist/js/comm.js
+++ 07-portfolio/bullbull/html-bullbull/dist/js/comm.js
@@ -56,4 +56,24 @@ $(function(){
   // count
   $(".counter").countimator();
 
+  // scrollUp
+  $.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: 'fade', // Fade, slide, none
+    animationInSpeed: 200, // Animation in speed (ms)
+    animationOutSpeed: 200, // Animation out speed (ms)
+    scrollText: '페이지를 위로 이동', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+  });
+
+  // slick
+  $('.slide').slick({
+    autoplay: true,
+    centerMode: true,
+  centerPadding: '300px',
+  slidesToShow: 1,
+  });
+
 })
